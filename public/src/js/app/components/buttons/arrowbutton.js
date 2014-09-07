faa6e7a97cb5d3b544c9253cfe42e7d9a5a7ad11
John <john.grindall@gmail.com>
2014-09-07 22:20:19 +0100

    Stop fades before I do them

--- public/src/js/app/components/buttons/arrowbutton.js
+++ public/src/js/app/components/buttons/arrowbutton.js
@@ -9,9 +9,10 @@ define('app/components/buttons/arrowbutton',['phasercomponents', 'app/assets'],
 		var frame0;
 		options.asset = Assets.BUTTON;
 		frame0 = 29 * 4 +  (4 * options.data.num);
-		options.frames = [frame0, frame0+1, frame0+2, frame0+3];
+		options.frames = [frame0, frame0 + 1, frame0 + 2, frame0 + 3];
 		PhaserComponents.Display.AbstractButton.call(this, options);
 		this.sprite.visible = options.data.visible;
+		this.stopTweens();
 	};
 	
 	ArrowButton.WIDTH = 50;
