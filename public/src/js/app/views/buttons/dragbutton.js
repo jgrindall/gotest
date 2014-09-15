a2b91280807ea95cdd2abfa1c8438dd43d4bd06a
John <john.grindall@gmail.com>
2014-09-15 19:35:42 +0100

    New button for creating draggers

---
+++ public/src/js/app/views/buttons/dragbutton.js
@@ -0,0 +1,25 @@
+
+define('app/views/buttons/dragbutton',[ 'phasercomponents', 'app/assets'],
+
+function(PhaserComponents, Assets){
+	
+	"use strict";
+	
+	var DragButton = function(options){
+		var frame0;
+		options.asset = Assets.DRAG_BUTTON;
+		frame0 = options.index;
+   		options.frames = [frame0, frame0 , frame0 , frame0];
+		PhaserComponents.Display.AbstractButton.call(this, options);
+	};
+
+	DragButton.WIDTH = 32;
+	DragButton.HEIGHT = 32;
+
+	PhaserComponents.Utils.extends(DragButton, PhaserComponents.Display.AbstractButton);
+
+	return DragButton;
+
+});
+
+
