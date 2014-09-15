87538625477a5bd4794ad4ae3a475066ec6fcb45
John <john.grindall@gmail.com>
2014-09-15 13:27:47 +0100

    gfx not used

--- public/src/js/app/views/components/indicator.js
+++ public/src/js/app/views/components/indicator.js
@@ -32,19 +32,14 @@ PhaserComponents, ModelFacade){
 		PhaserComponents.Display.Container.prototype.destroy.call(this);
 		ModelFacade.getInstance().get(ModelFacade.COMMTICKER).changeSignal.remove(this.setProgress, this);
 		ModelFacade.getInstance().get(ModelFacade.COMM).changeSignal.remove(this.setProgress, this);
-		this.group.remove(this.gfx);
 		this.group.remove(this.label);
-		this.gfx.destroy();
 		this.label.destroy();
-		this.gfx = null;
 		this.label = null;
 	};
 
 	Indicator.prototype.create = function(){
 		PhaserComponents.Display.Container.prototype.create.call(this);
 		this.label = PhaserComponents.TextFactory.make('vsmall', this.game, this.bounds.x, this.bounds.y, "0/0");
-		this.gfx = new Phaser.Graphics(this.game, this.options.bounds.x, this.options.bounds.y);
-		this.group.add(this.gfx);
 		this.group.add(this.label);
 	};
 	
