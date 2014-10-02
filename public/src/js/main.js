186adaf5808e02077b9d9f934fceb70542797c06
John <john.grindall@gmail.com>
2014-10-02 11:57:04 +0100

    No minHeight/width

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -15,7 +15,7 @@ require(['phaser'], function(Phaser){
 	
 		$(document).ready(function(){
 			var options, app;
-			options = {"containerTagId":"game", "scaleType":"fill", "minWidth":800, "minHeight":620, "paddingBottom":45};
+			options = {"containerTagId":"game", "scaleType":"fill", "paddingBottom":45};
 			app = new AppContext(options);
 			app.start();
 		});
@@ -23,25 +23,5 @@ require(['phaser'], function(Phaser){
 	});
 
 });
-
-
-/**
-1360x768	2
-1920x1200	3
-1680x1050	5
-1600x900	6
-1024x768	6
-1440x900	7
-1280x800	7
-1280x1024	8
-1920x1080	13
-1366x768	31
-**/
-
-
-// Smallest: 1024 x 768
-
-// Largest: 1920 x 1200
-
 
 
