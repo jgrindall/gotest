04eaf27c5ac43831b3c716fc2d7fa17272c8ed56
John <john.grindall@gmail.com>
2014-10-21 16:52:44 +0100

    debugging

--- public/src/js/app/commands/openbgeditorcommand.js
+++ public/src/js/app/commands/openbgeditorcommand.js
@@ -17,7 +17,10 @@ function(PhaserComponents,
 	PhaserComponents.Utils.extends(OpenBgEditorCommand, PhaserComponents.Commands.AbstractCommand);
 
 	OpenBgEditorCommand.prototype.onImgSelected = function(base64Data){
+		var was = this.modelFacade.get(ModelConsts.BG_PNG).get();
 		console.log("bg command", base64Data);
+		console.log("was", was);
+		console.log(base64Data === was);
 		this.modelFacade.get(ModelConsts.BG_PNG).set(base64Data);
 	};
 
