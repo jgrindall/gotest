5186ec0aca59a0825629c50d9dab6bae89c97aa3
John <john.grindall@gmail.com>
2014-10-27 13:21:44 +0000

    New turtle and menu

---
+++ public/src/js/app/views/popups/gridmenu.js
@@ -0,0 +1,103 @@
+
+define(['app/assets',
+
+'phasercomponents', 'app/models/modelconsts',
+
+'app/views/buttons/okbuttoncontainer',
+
+'app/views/buttons/closebutton',
+
+'app/views/buttons/tabbutton',
+
+'app/views/popups/gamescreenmenupanel', 'app/views/popups/settingsmenupanel'],
+
+function(Assets,
+
+PhaserComponents, ModelConsts,
+
+OkButtonContainer,
+
+CloseButton,
+
+TabButton,
+
+GameScreenMenuPanel, SettingsMenuPanel){
+	
+	"use strict";
+		
+	var GridMenu = function(options){
+		options.bgasset = Assets.PANEL;
+		PhaserComponents.Display.AbstractPopup.call(this, options);
+	};
+	
+	GridMenu.WIDTH = 	720;
+	GridMenu.HEIGHT = 	540;
+
+	PhaserComponents.Utils.extends(GridMenu, PhaserComponents.Display.AbstractPopup);
+	
+	GridMenu.prototype.create = function () {
+		PhaserComponents.Display.AbstractPopup.prototype.create.call(this);
+		this.addTab();
+		this.addCloseButton();
+		this.addOkButton();
+		this.addLabel();
+	};
+
+	GridMenu.prototype.addTab = function () {
+		var options, bounds, panels, panel0, panel1, screenModel, radioModel;
+		bounds = this.bounds;
+		screenModel = new PhaserComponents.Model.ButtonGridModel();
+		radioModel = new PhaserComponents.Model.ButtonGridModel();
+		screenModel.set(this.modelFacade.get(ModelConsts.SCREEN).get());
+		radioModel.set(this.modelFacade.get(ModelConsts.ANGLE).get());
+		panel0 = new GameScreenMenuPanel({"bounds":this.bounds, "screenModel":screenModel, "radioModel":radioModel, "sfx":Assets.SOUNDS[2]});
+		panel1 = new SettingsMenuPanel({"bounds":this.bounds});
+		panels = [panel0, panel1];
+		options = {"bounds":bounds, "panels":panels, "buttonClass":TabButton};
+		this.tabPanel = new PhaserComponents.Display.TabPanel(options);
+		this.group.add(this.tabPanel.view);
+		this.tabPanel.buttonBar.view.x += 140;
+		this.tabPanel.buttonBar.view.y += 11;
+	};
+
+	GridMenu.prototype.getData = function () {
+		return this.tabPanel.getData();
+	};
+
+	GridMenu.prototype.addLabel = function () {
+		this.title = PhaserComponents.TextFactory.make('medium', this.game, this.bounds.x + 16, this.bounds.y + 10, "Settings");
+		this.group.add(this.title);
+	};
+
+	GridMenu.prototype.addOkButton = function () {
+		var middle = this.bounds.x + this.bounds.w/2 - (OkButtonContainer.WIDTH/2);
+		var bounds = {"x":middle, "y":this.bounds.y + this.bounds.h - OkButtonContainer.HEIGHT + 3};
+		this.addButton(OkButtonContainer, bounds);
+	};
+
+	GridMenu.prototype.addCloseButton = function () {
+		var bounds = {"x":this.bounds.x + this.bounds.w - CloseButton.WIDTH, "y":this.bounds.y };
+		this.addButton(CloseButton, bounds);
+	};
+
+	GridMenu.prototype.destroy = function() {
+		this.group.remove(this.tabPanel.view);
+		this.group.remove(this.title);
+		this.tabPanel.destroy();
+		this.tabPanel = null;
+		this.title.destroy();
+		this.title = null;
+		PhaserComponents.Display.AbstractPopup.prototype.destroy.call(this);
+	};
+	
+	return GridMenu;
+	
+});
+	
+
+
+
+
+
+
+
