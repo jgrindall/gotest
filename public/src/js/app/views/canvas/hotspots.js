85998015b571f8aefce2998fdc51ca726d8701b3
John <john.grindall@gmail.com>
2014-11-04 13:10:30 +0000

    adding hotspots

--- public/src/js/app/views/canvas/hotspots.js
+++ public/src/js/app/views/canvas/hotspots.js
@@ -20,18 +20,16 @@ PhaserComponents, Assets){
 	PhaserComponents.Utils.extends(Hotspots, PhaserComponents.Display.Container);
 
 	Hotspots.prototype.addHotspot = function() {
-		//this.hotGroup = new Phaser.Group(this.game, null);
 		this.gfx = new Phaser.Graphics(this.game, 0, 0);
-		//this.hotGroup.add(this.gfx);
-		this.gfx.lineStyle(0, 0x000000, 0);
+		this.gfx.lineStyle(4, 0xF3E5AB, 0.5);
 		this.gfx.beginFill(0xc8c8c8, 0.3);
-		this.gfx.drawCircle(300, 300, 90);
+		this.gfx.drawCircle(0, 0, 90);
 		this.gfx.endFill();
-		//this.group.add(this.hotGroup);
-		this.gfx.pivot.setTo(0.5, 0.5);
+		this.gfx.pivot.setTo(45, 45);
 		this.group.add(this.gfx);
-		//this.hotGroup.pivot.setTo(0.5, 0.5);
-		this.pulse = this.game.add.tween(this.gfx.scale).to( {'x': 1.2, 'y':1.2}, 1000, Phaser.Easing.Linear.None, true, 0, true, true);
+		this.gfx.x = 300;
+		this.gfx.y = 300;
+		this.pulse = this.game.add.tween(this.gfx.scale).to( {'x': 1.025, 'y':1.025}, 300, Phaser.Easing.Linear.None, true, 0, Number.MAX_VALUE, true);
 	};
 
 	Hotspots.prototype.create = function() {
