d09e1e418778f4a6d1760a4a6c5a6e40b6686e28
John <john.grindall@gmail.com>
2014-11-10 16:48:34 +0000

    removed window

--- public/src/js/app/utils/purplemashclipartadapter.js
+++ public/src/js/app/utils/purplemashclipartadapter.js
@@ -34,7 +34,7 @@ define(['phasercomponents', 'app/utils/abstractclipartadapter', 'app/utils/error
 				try{
 					window.ClipArtHelper.getImage(id, function(data){
 						src = data.src;
-						window.alert("src "+src.substring(0, 50)+"   " +that.isBase64(src));
+						//window.alert("src "+src.substring(0, 50)+"   " +that.isBase64(src));
 						if(that.isBase64(src)){
 							options.success(src);
 						}
@@ -93,7 +93,7 @@ define(['phasercomponents', 'app/utils/abstractclipartadapter', 'app/utils/error
 	        	canvas.width = img.width;
 	        	context.drawImage(img, 0, 0);
 	        	dataURL = canvas.toDataURL("image/png");
-	        	window.alert("imgUrlToBase64 dataURL "+dataURL.substring(0,50));
+	        	//window.alert("imgUrlToBase64 dataURL "+dataURL.substring(0,50));
 	        	options.success(dataURL);
 	        	canvas = null;
 	        	context = null;
