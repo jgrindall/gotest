ec3f3f0eec0c68e915bcde81f82878e7f6c19dbb
John <john.grindall@gmail.com>
2014-11-14 15:19:36 +0000

    Show once only

--- public/src/js/app/commands/checkoutsidecommand.js
+++ public/src/js/app/commands/checkoutsidecommand.js
@@ -14,6 +14,8 @@ function(PhaserComponents,
 		PhaserComponents.Commands.AbstractCommand.call(this);
 	};
 	
+	CheckOutsideCommand.popupShown = false;
+
 	PhaserComponents.Utils.extends(CheckOutsideCommand, PhaserComponents.Commands.AbstractCommand);
 
 	CheckOutsideCommand.prototype.onClick = function(){
@@ -24,8 +26,11 @@ function(PhaserComponents,
 		var bounds, msg;
 		msg = "Oops, it looks like you've gone off\nthe screen.  To get back to the start\nyou can the click rewind button.";
 		if(data.x < -Turtles.WIDTH/2 || data.x > CanvasLayout.REF_WIDTH + Turtles.WIDTH/2 || data.y < -Turtles.HEIGHT/2 || data.y > CanvasLayout.REF_HEIGHT + Turtles.HEIGHT/2){
-			bounds = {'x':this.game.w - 610, 'y':28};
-			this.alertManager.make(ToolTip, {"name":"outside", "title":"Message", "label":msg, "arrow":4, "imageAsset":null}, this.onClick.bind(this), bounds);
+			if(!CheckOutsideCommand.popupShown){
+				bounds = {'x':this.game.w - 610, 'y':28};
+				this.alertManager.make(ToolTip, {"name":"outside", "title":"Message", "label":msg, "arrow":4, "imageAsset":null}, this.onClick.bind(this), bounds);
+				CheckOutsideCommand.popupShown = true;
+			}
 		}
 		else{
 			this.alertManager.close(null, "outside");
