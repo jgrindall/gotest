93d0bd5614fd5eedd9ea2c4676eaa7c91e4bceab
John <john.grindall@gmail.com>
2014-11-17 15:41:46 +0000

    new grunt file with new files

--- Gruntfile.js
+++ Gruntfile.js
@@ -106,7 +106,7 @@ module.exports = function(grunt) {
             main: {
                 expand:true,
                 cwd: 'public/src/', 
-                src: ['assets/**/*.png','assets/**/*.jpg','assets/**/*.gif','assets/**/*.mp3','assets/**/*.wav','assets/**/*.ttf','assets/**/*.json'],
+                src: ['assets/**/*.png','assets/**/*.jpg','assets/**/*.gif','assets/**/*.mp3','assets/**/*.wav','assets/**/*.ogg','assets/**/*.ttf','assets/**/*.eot','assets/**/*.woff','assets/**/*.json'],
                 dest: 'public/build/'
             }
         }
