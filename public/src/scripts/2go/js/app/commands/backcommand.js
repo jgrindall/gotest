5fbff963cfecb40fa966b28741b32958984a5585
John <john.grindall@gmail.com>
2015-04-28 14:51:56 +0100

    New exit code

--- public/src/scripts/2go/js/app/commands/backcommand.js
+++ public/src/scripts/2go/js/app/commands/backcommand.js
@@ -13,22 +13,8 @@ function(PhaserComponents) {
 	PhaserComponents.Utils.extends(BackCommand, PhaserComponents.Commands.AbstractCommand);
 
 	BackCommand.prototype.execute = function(){
-		if(window.closeApplication && window.pmConfirm && window._T){
-			window.pmConfirm({
-    			text: window._T('Are you sure you want to exit this program') + '?',
-    			title: window._T('Exit') + "?",
-    			onOk: window.closeApplication,
-    			useNodder: true,
-    			width: "250",
-    			className: "codeDialog"
-			});
-		}
-		else if(window.closeApplication && typeof window.closeApplication === 'function'){
-			window.closeApplication();
-		}
-		else{
-			//console.log("window.closeApplication is "+window.closeApplication);
-			//window.history.back();
+		if(window.pmExit){
+			window.pmExit();
 		}
 	};
 	
