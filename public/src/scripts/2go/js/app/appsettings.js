8856d69579ecafca1a7230703491581e1ea3b9a6
John <john.grindall@gmail.com>
2018-01-08 14:29:38 +0000

    not live

--- public/src/scripts/2go/js/app/appsettings.js
+++ public/src/scripts/2go/js/app/appsettings.js
@@ -12,10 +12,10 @@ define([],
     regexp0 = new RegExp('purple', 'g');
     regexp1 = new RegExp('simple', 'g');
 
-	AppSettings.LIVE = (regexp0.test(window.location) || regexp1.test(window.location));
+	AppSettings.LIVE = false;
 	
+	//AppSettings.LIVE = true;
 	// IMPORTNAT: override this to true once it has gone live!
-	AppSettings.LIVE = true;
 
 	return AppSettings;
 	
