ccfb0826cc15acbbab165b7e3f7109f72b55d742
John <john.grindall@gmail.com>
2018-02-07 09:50:04 +0000

    hide irrelevant buttons

--- public/src/scripts/2go/js/app/views/components/menu.js
+++ public/src/scripts/2go/js/app/views/components/menu.js
@@ -15,16 +15,16 @@ PlayingState, ModelConsts){
 	
 	var Menu  = function(options){
 		options.buttonClass = MenuButton;
-		options.numX = 5;
+		options.numX = 2;
 		options.numY = 1;
-		options.data = [{'num':0}, {'num':1}, {'num':2}, {'num':3}, {'num':4}];
+		options.data = [{'num':0}, {'num':3}];
 		PhaserComponents.Display.ButtonBar.call(this, options);
 		this.eventDispatcher.addListener(PhaserComponents.Events.AppEvents.ALERT_SHOWN, this.onAlert.bind(this));
 		this.modelFacade.get(ModelConsts.PLAYING).changeSignal.add(this.playingChanged, this);
 		this.clickSignal.add(this.menuClick, this);
 	};
 	
-	Menu.WIDTH = 230;
+	Menu.WIDTH = 90;
 	Menu.HEIGHT = 50;
 
 	PhaserComponents.Utils.extends(Menu, PhaserComponents.Display.ButtonBar);
@@ -44,16 +44,7 @@ PlayingState, ModelConsts){
 			this.eventDispatcher.trigger({"type":Events.NEW_FILE});
 		}
 		else if(i === 1){
-			this.eventDispatcher.trigger({"type":Events.LOAD});
-		}
-		else if(i === 2){
-			this.eventDispatcher.trigger({"type":Events.SAVE});
-		}
-		else if(i === 3){
 			this.eventDispatcher.trigger({"type":Events.DOWNLOAD});
-		}
-		else if(i === 4){
-			this.eventDispatcher.trigger({"type":Events.SHARE});
 		}
 	};
 
