cb37ea4271081e34595b4387df2abf6eb12ecacd
John Grindall <john@2simple.com>
2019-02-19 10:21:33 +0000

    remove Entity stuff

--- public/src/scripts/2go/js/app/consts/defaults.js
+++ public/src/scripts/2go/js/app/consts/defaults.js
@@ -1,5 +1,5 @@
 
-define(['2go/app/consts/challengedata', '2go/app/appsettings'], function(ChallengeData, AppSettings) {
+define(['base/consts/challengedata', 'base/appsettings'], function(ChallengeData, AppSettings) {
 
 	"use strict";
 
@@ -31,14 +31,7 @@ define(['2go/app/consts/challengedata', '2go/app/appsettings'], function(Challen
 
 	Defaults.getDefaults = function(){
 		var settings = $.extend({}, Defaults.DEFAULT_SETTINGS);
-		// these names don't quite match sorry...
-		settings.angle = 			Math.max(["45 degrees", "90 degrees"].indexOf(AppSettings.ENTITY_SETTINGS.rotationAngle), 0);
-		settings.allowProg = 		AppSettings.ENTITY_SETTINGS.allowProgramming ? 1 : 0;
-		settings.screen = 			Math.max(["Simple", "Number keys", "Include diagonals", "Turn on the spot"].indexOf(AppSettings.ENTITY_SETTINGS.inputType), 0);
-		settings.grid = 			AppSettings.ENTITY_SETTINGS.showGrid ? 1 : 0;
-		settings.stepLength = 		Math.max(["Very small", "Small", "Medium", "Large"].indexOf(AppSettings.ENTITY_SETTINGS.stepLength), 0);
-		settings.diag = 			AppSettings.ENTITY_SETTINGS.stretchDiagonals ? 1 : 0;
-		console.log("settings", settings);
+		
 		return {
 			'settings':settings,
 			'commands':[],
